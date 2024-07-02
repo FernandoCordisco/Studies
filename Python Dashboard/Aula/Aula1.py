44983d9256a1b1617af9074f838e93c9a1149946
FernandoCordisco <fernandorcvilela@hotmail.com>
2024-07-02 00:06:16 -0300

    Ajustando repositório

--- Python Dashboard/Aula/Aula1.py
+++
@@ -1,8 +0,0 @@
-#Aula 1
-
-print('Hello Word')
-
-print(1 + 2)
-
-print(type(0.5))
-
--- Python Dashboard/Runner.py
+++
@@ -1,5 +0,0 @@
-import sys
-from streamlit.web import cli as stcli
-
-sys.argv = ["streamlit", "run", "BookHome.py"]
-sys.exit(stcli.main())
