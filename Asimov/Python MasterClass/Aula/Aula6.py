4a344fd38f610ec4f007710be20be0953d7a7349
Fernando Rodrigues Cordisco Vilela <fernando.vilela@totvs.com.br>
2024-07-11 21:50:03 -0300

    --

---
+++ Asimov/Python MasterClass/Aula/Aula6.py
@@ -0,0 +1,18 @@
+#Aula 6
+
+#For
+
+a = [1, 2 , 3, 4, 5, 6, 7]
+b = 0
+
+for x in a :
+    print(x)
+    b = b + x
+
+print(b)
+
+for x in range(0, 10, 2):
+    print(x)
+
+for x in range(10):
+    print(x)
